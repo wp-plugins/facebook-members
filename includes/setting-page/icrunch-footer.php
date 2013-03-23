8f7ac145712f14f114156602593b212d5d34b42e
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-23 15:51:53 +0000

    = 5.0.3 = * Bug Fixes * Fixed ReadMe Typo * DomainName Change * Author's Twitter link fixed
    git-svn-id: https://plugins.svn.wordpress.org/facebook-members/tags/5.0.3@686290 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/icrunch-footer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author Crunchify.co
+ * Plugin: Facebook Members
+ */
+?>
+
+
+</div>
+</div>
+<div class="clear">
+    <p>
+        <br/>&copy; Copyright 2007 - <?php echo date("Y"); ?> <a href="http://Crunchify.co" target="_blank">Crunchify.co</a>
+    </p>
+</div>		
---
+++ includes/setting-page/icrunch-right-panel2.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Crunchify.co
+ * Plugin: Facebook Members
+ */
+?>
+
+<br>
+<div id="side-info-column" class="inner-sidebar">
+    <div class="postbox">
+        <h3 class="hndle"><span>My Other Plugins</span></h3>
+
+        <div class="inside">
+            <ul>
+                <li>1) <a href="http://Crunchify.co/twitter-goodies/" title="Twitter Goodies" target="_blank">Twitter
+                    Goodies</a></li>
+                <li>2) <a href="http://Crunchify.co/wp-google-buzz/" title="All in One Webmaster" target="_blank">WP
+                    Google Buzz</a></li>
+                <li>3) <a href="http://Crunchify.co/wp-archive-sitemap-generator/" title="WP Archive-Sitemap Generator"
+                          target="_blank">WP Archive-Sitemap Generator</a></li>
+                <li>4) <a href="http://Crunchify.co/foursquare-checkins/" title="FourSquare Checkins"
+                          target="_blank">FourSquare Checkins</a></li>
+                <li>5) <a href="http://Crunchify.co/all-in-one-webmaster/" title="Facebook Members" target="_blank">All in
+                    One Webmaster</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
