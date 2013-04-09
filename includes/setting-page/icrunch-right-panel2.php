f429426012b8ce15562a42f4bdb7c53464ec6a70
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-04-09 05:13:38 +0000

    = 5.0.4 = * Fixed some broken links * Screenshots update
    git-svn-id: https://plugins.svn.wordpress.org/facebook-members/tags/5.0.4@694270 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/icrunch-right-panel2.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Crunchify.com
+ * Plugin: Facebook Members
+ */
+?>
+
+<br>
+<div id="side-info-column" class="inner-sidebar">
+    <div class="postbox">
+        <h3 class="hndle"><span>My Other Plugins</span></h3>
+
+        <div class="inside">
+            <ul>
+                <li>1) <a href="http://Crunchify.com/twitter-goodies/" title="Twitter Goodies" target="_blank">Twitter
+                    Goodies</a></li>
+                <li>2) <a href="http://Crunchify.com/wp-google-buzz/" title="All in One Webmaster" target="_blank">WP
+                    Google Buzz</a></li>
+                <li>3) <a href="http://Crunchify.com/wp-archive-sitemap-generator/" title="WP Archive-Sitemap Generator"
+                          target="_blank">WP Archive-Sitemap Generator</a></li>
+                <li>4) <a href="http://Crunchify.com/foursquare-checkins/" title="FourSquare Checkins"
+                          target="_blank">FourSquare Checkins</a></li>
+                <li>5) <a href="http://Crunchify.com/all-in-one-webmaster/" title="Facebook Members" target="_blank">All in
+                    One Webmaster</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
