773dc8ee4e9b2da71d07c07f55d7cbd172b9e834
Crunchify <Crunchify@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-01 18:55:36 +0000

    * setting page - modified subpage names * removed number of connections options * fixed recommendation bar script * facebook like box - URL changed to https * removed border_color option from script * added show_border=false to script * better CSS class added * fixed broken sidebar logo
    git-svn-id: https://plugins.svn.wordpress.org/facebook-members/tags/5.5@958840 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/setting-page/crunchify-right-panel2.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author Crunchify.com
+ * Plugin: Facebook Members
+ */
+?>
+
+<br>
+<div id="side-info-column" class="inner-sidebar">
+    <div class="postbox">
+        <h3 class="hndle"><span>All in One Webmaster Version Features</span></h3>
+
+        <div class="inside">
+            <ul>
+                <li>1) Brand new dedicated plugin menu</li>
+                <li>2) Facebook page insights, admin insights, app insights options</li>
+                <li>3) Google XML Sitemap (sitemap.xml)</li>
+                <li>4) Google Authorship Options</li>
+                <li>5) Google Tag Manager Integration</li>
+                <li>6) Header / Footer Section</li>
+                <li>7) Misc Options: Add Global Site Favicon Option</li>
+                <li>8) And lot more...</li>
+            </ul>
+        </div>
+    </div>
+</div>
